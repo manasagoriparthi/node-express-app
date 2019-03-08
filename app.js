bb439de7024d39274bafafba206042b4b6b81884
manasagoriparthi <46694230+manasagoriparthi@users.noreply.github.com>
2019-03-08 10:57:09 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-const hostname = '127.0.0.1'   // set constants
+const hostname = '0.0.0.0'   // set constants
 const port = 3002
 
+app.use(express.static(path.join(__dirname, './styles')))
+
 app.get('/', function (req, res) {
-  res.send('Welcome home!')
+  //res.send('Welcome home!')
+  res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('/hello', (req, res) => {
@@ -16,12 +20,12 @@ app.get('/big',  (req, res) =>{
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/greeting/:id',  (req, res) =>{
-  res.send('Hello! The id was ' + req.params.id)
+app.get('/greeting',  (req, res) =>{
+  res.send('Hello! Nice to meet you.')
 })
 
-app.get('/yo/:buddy',  (req, res) =>{
-  res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
+app.get('/yo/buddy',  (req, res) =>{
+  res.send('<h1>Yo, buddy!</h1>')
 })
 
 // handle non-existant routes
@@ -33,4 +37,3 @@ app.listen(port, hostname, () => {
   console.log(`Example app listening at http://${hostname}:${port}/`)
   console.log('Hit CTRL-C CTRL-C to stop\n')
 })
-
